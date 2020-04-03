380a1111a92a2aaab2417c96731148bedf3b61b5
laurie <laurie@tenmilesquare.com>
2020-04-03 10:24:09 -0400

    update README

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    let obj = { node: 1 }
+    console.log(obj.node)
+    console.log(obj?.node)
+    let other = null
+    //other.node will error
+    console.log(other?.node)
+  })
+
+  return <div className="App">Check the console.</div>
 }
 
-export default App;
+export default App
